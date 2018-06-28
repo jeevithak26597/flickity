f859cfd8202b68ee7b2caf50868f99849fca983e
chandanajampala <jampala.c@gmail.com>
2018-06-29 03:29:14 +0530

    backend code for updateDetails

--- src/app/registration/pages/login/login.component.ts
+++ src/app/registration/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { ValidationErrors } from '@angular/forms';
 import {AbstractControl} from '@angular/forms';
 import { ServiceregisterService } from '../../serviceregister.service';
 import { Router } from '@angular/router';
+import { CommonServiceService } from '../../../common-service.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,7 +14,7 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   idformgroup:FormGroup;
 router;
-  constructor(public service:ServiceregisterService,public r:Router) { 
+  constructor(public service:ServiceregisterService,public r:Router,public commonser:CommonServiceService) { 
     this.service.fetchUser();
     this.router=r;
     this.idformgroup=new FormGroup({
@@ -27,6 +28,7 @@ login(){
      if(this.service.usrArr[i].mail==this.idformgroup.value.mailid){
        if(this.service.usrArr[i].password==this.idformgroup.value.password){
         loged=true;
+        this.commonser.setUser(this.service.usrArr[i].key);
          if(this.service.usrArr[i].role=="admin"){
           
            this.router.navigate(['admin/dashboard']);
